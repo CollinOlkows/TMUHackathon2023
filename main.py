b5499f92baf95157dcf063904bda57a641d5b4ad
CollinOlkows <99486132+CollinOlkows@users.noreply.github.com>
2023-05-07 13:36:16 +0000

    Finished

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import csv
 
 @bottle.route("/")
 def index():
-    return bottle.static_file("index.html", root='.')
+    return bottle.static_file("Home.html", root='.')
 @bottle.route("/instagram")
 def instagram():
     return bottle.static_file("instagram.html", root='.')
@@ -335,7 +335,115 @@ def post80():
   return bottle.static_file("/Questions/Type 1 - Bot or Not/bot - 5/pfp.jpg",root='.')
 
 
+@bottle.route('/Team.html')
+def idc1():
+  return bottle.static_file("/Team.html",root='.')
+@bottle.route('/Returning Players.html')
+def idc2():
+  return bottle.static_file("/Returning Players.html",root='.')
+@bottle.route('/Our Mission and Goals.html')
+def idc3():
+  return bottle.static_file("/Our Mission and Goals.html",root='.')
+@bottle.route('/Leader Board.html')
+def idc4():
+  return bottle.static_file("/Leader Board.html",root='.')
+@bottle.route('/Home.html')
+def idc5():
+  return bottle.static_file("/Home.html",root='.')
+@bottle.route('/Game.html')
+def idc6():
+  return bottle.static_file("/Game.html",root='.')
+@bottle.route('/Blank 1.html')
+def idc7():
+  return bottle.static_file("/Blank 1.html",root='.')
+@bottle.route('/Blank 2.html')
+def idc8():
+  return bottle.static_file("/Blank 2.html",root='.')
+@bottle.route('/About.html')
+def idc9():
+  return bottle.static_file("/About.html",root='.')
+@bottle.route('/Play Now.html')
+def idc10():
+  return bottle.static_file("/Play Now.html",root='.')
 
+@bottle.route('/Team/917d6f86a9afc3640bf04951cd61f707.jpg')
+def idc11():
+  return bottle.static_file("/Team/917d6f86a9afc3640bf04951cd61f707.jpg",root='.')
+
+@bottle.route('/Team/27d54c0ecc85dc46c2333716da274549.jpg')
+def idc12():
+  return bottle.static_file("/Team/27d54c0ecc85dc46c2333716da274549.jpg",root='.')
+
+@bottle.route('/Team/b81a05c8d45ae618dc194660b1bdcd84.jpg')
+def idc13():
+  return bottle.static_file("/Team/b81a05c8d45ae618dc194660b1bdcd84.jpg",root='.')
+  
+@bottle.route('/Team/c1ae1504910fb5ab862e70044e8b31d3.jpg')
+def idc14():
+  return bottle.static_file("/Team/c1ae1504910fb5ab862e70044e8b31d3.jpg",root='.')
+  
+@bottle.route('/Team/dd31e5de947b8893eafd0af61db4a2a6.jpg')
+def idc15():
+  return bottle.static_file("/Team/dd31e5de947b8893eafd0af61db4a2a6.jpg",root='.')
+  
+@bottle.route('/Team/eceb2e1af111094ba6c1cbfde153eadf.jpg')
+def idc16():
+  return bottle.static_file("/Team/eceb2e1af111094ba6c1cbfde153eadf.jpg",root='.')
+
+@bottle.route('/Our Mission and Goals/277cdd526e6ceaea1ada061fb0e30ba5.jpg')
+def idc17():
+  return bottle.static_file("/Our Mission and Goals/277cdd526e6ceaea1ada061fb0e30ba5.jpg",root='.')
+
+@bottle.route('/Our Mission and Goals/4926316e7e4022ec70ba41d45bad5131.jpg')
+def idc18():
+  return bottle.static_file("/Our Mission and Goals/4926316e7e4022ec70ba41d45bad5131.jpg",root='.')
+
+@bottle.route('/Our Mission and Goals/aebfde07b6ae86654fbc11ed12775cf5.jpg')
+def idc19():
+  return bottle.static_file("/Our Mission and Goals/aebfde07b6ae86654fbc11ed12775cf5.jpg",root='.')
+
+@bottle.route('/Our Mission and Goals/f05eda15ec48f71715ffce669c5a1114.jpg')
+def idc20():
+  return bottle.static_file("/Our Mission and Goals/f05eda15ec48f71715ffce669c5a1114.jpg",root='.')
+
+
+@bottle.route('/Home/1b6d85e6ebeee04ac69ce8b9f700cf2a.jpg')
+def idc21():
+  return bottle.static_file("/Home/1b6d85e6ebeee04ac69ce8b9f700cf2a.jpg",root='.')
+
+@bottle.route('/Home/6dd435f5031b34febd41b2cce9a1ab8b.jpg')
+def idc22():
+  return bottle.static_file("/Home/6dd435f5031b34febd41b2cce9a1ab8b.jpg",root='.')
+
+@bottle.route('/Home/054d44ff114c768607d9b138176b5f04.jpg')
+def idc23():
+  return bottle.static_file("/Home/054d44ff114c768607d9b138176b5f04.jpg",root='.')
+
+@bottle.route('/Home/224a76b350745e2098a09a13f8d06bb6.jpg')
+def idc24():
+  return bottle.static_file("/Home/224a76b350745e2098a09a13f8d06bb6.jpg",root='.')
+
+@bottle.route('/Home/788b04872c8999e6f07c2baefac14506.jpg')
+def idc25():
+  return bottle.static_file("/Home/788b04872c8999e6f07c2baefac14506.jpg",root='.')
+
+@bottle.route('/About/8ecf02110c866da69bf9423681b470bf.jpg')
+def idc26():
+  return bottle.static_file("/About/8ecf02110c866da69bf9423681b470bf.jpg",root='.')
+
+@bottle.route('/About/238720247980ea5459e3f6318a811fd3.jpg')
+def idc27():
+  return bottle.static_file("/About/238720247980ea5459e3f6318a811fd3.jpg",root='.')
+@bottle.route('/917d6f86a9afc3640bf04951cd61f707.jpg')
+def idc27():
+  return bottle.static_file("/917d6f86a9afc3640bf04951cd61f707.jpg",root='.')
+
+
+  
+  
+  
+  
+  
 
 
 
