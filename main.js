8384b429b8a6c903f7793234597e5b157daf51f7
CheezItMasterRace <7354953-CheezItMasterRace@users.noreply.replit.com>
2023-05-07 11:43:49 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,219 @@
+function ajaxGetRequest(path, callback){
+    let request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if (this.readyState === 4 && this.status === 200){
+            callback(this.response);
+        }
+    };
+    request.open("GET", path);
+    request.send();
+}
+
+function ajaxPostRequest(path, data, callback){
+    let request = new XMLHttpRequest();
+    request.onreadystatechange = function(){
+        if (this.readyState === 4 && this.status === 200){
+            callback(this.response);
+        }
+    };
+    request.open("POST", path);
+    request.send(data);
+}
+
+
+
+
+cycles=0
+n=-1
+profiles=[]
+Image=0
+score=0
+
+function home(){
+  
+  //Reset all variables
+  console.log('Done')
+}
+
+//making a guess its a real account
+function real(){
+  if(document.getElementById("Bot_Button").style.visibility == 'visible'){
+    if(cycles<5){
+    console.log('Guessed Real');
+    cycles+=1;
+      if(profiles[num][0]=='not '){
+        score+=100
+        console.log(score)
+        console.log('here')
+        document.getElementById("Score").innerHTML=score
+      }
+    let id=document.getElementById("Bot_Button");
+    id.style.visibility="hidden";
+    id=document.getElementById("Next");
+    id.style.visibility="visible";
+    id=document.getElementById("Exp");
+      id=document.getElementById("Exp");
+    document.getElementById("Exp").innerHTML=profiles[num][26]
+    id.style.visibility="visible";
+  }else{
+    let id=document.getElementById("Leaderboards");
+    id.style.visibility="visible"
+      id=document.getElementById("Exp");
+      id.style.visibility="visible"
+
+  }
+  }else{
+    console.log('nahhhh')
+  }
+  
+}
+
+//making the guess its a fake account
+function Bot(){
+  if(document.getElementById("Real_Button").style.visibility == 'visible'){
+    if(cycles<5){
+    console.log('Guessed Bot');
+    console.log(cycles)
+      if(profiles[num][0]=='bot '){
+        score+=100
+        console.log(score)
+        document.getElementById("Score").innerHTML=score
+      }
+    let id=document.getElementById("Real_Button");
+    id.style.visibility="hidden"
+    id=document.getElementById("Next");
+    id.style.visibility="visible"
+      ///if correct update score value else dont
+    id=document.getElementById("Exp");
+    document.getElementById("Exp").innerHTML=profiles[num][26]
+    id.style.visibility="visible"
+    cycles+=1;
+  }else{
+    let id=document.getElementById("Leaderboards");
+    id.style.visibility="visible"
+      id=document.getElementById("Exp");
+      id.style.visibility="visible"
+  }
+  }else{
+    console.log('nah')
+  }
+  
+}
+
+//update the leaderboard info
+function Leaderboard(){
+
+  
+}
+
+function Next(){
+  let id=document.getElementById("Real_Button");
+  id.style.visibility="visible";
+  id=document.getElementById("Bot_Button");
+  id.style.visibility="visible";
+  id=document.getElementById("Exp");
+  id.style.visibility="hidden";
+  id=document.getElementById("Next");
+  id.style.visibility="hidden";
+  SetupProfile()
+}
+function LoadData(){
+  ajaxGetRequest('/data',Loaded)
+}
+function SetupProfile(){
+  if (n > -1) { 
+    profiles.splice(n, 1); 
+  }
+  num=Math.floor(Math.random() * profiles.length);
+  n=num
+  console.log(n)
+  id=document.getElementById("pfp");
+  console.log(num)
+  s=profiles[num]
+  console.log(s)
+  id.src=profiles[num][27]
+  id=document.getElementById("Exp");
+    document.getElementById("Exp").innerHTML=profiles[num][26]
+  id=document.getElementById("Image1");
+  id.src=profiles[num][8]
+  id=document.getElementById("Image2");
+  id.src=profiles[num][11]
+  id=document.getElementById("Image3");
+  id.src=profiles[num][14]
+  id=document.getElementById("Image4");
+  id.src=profiles[num][17]
+  id=document.getElementById("Image5");
+  id.src=profiles[num][20]
+  id=document.getElementById("Image6");
+  id.src=profiles[num][23]
+  id=document.getElementById("Profile_Name").innerHTML='@'+profiles[num][1]
+  id=document.getElementById("Name").innerHTML=profiles[num][2]
+  id=document.getElementById("Bio").innerHTML=profiles[num][7]
+  id=document.getElementById("Web").innerHTML=profiles[num][3]
+  id=document.getElementById("Profile_Posts_Count").innerHTML=profiles[num][4]
+  id=document.getElementById("Profile_Followers_Count").innerHTML=profiles[num][5]
+  id=document.getElementById("Profile_Following_count").innerHTML=profiles[num][6]
+  return
+  
+
+}
+
+function Zoom(){
+  ajaxGetRequest('/currentData',LoadedC)
+  
+}
+
+function LoadedC(response){
+  let d = JSON.parse(response);
+  let s = JSON.parse(response);
+  console.log(d)
+  id=document.getElementById("Img");
+  if(s[0][0]=='1'){
+    id.src=d[0][10];
+    id.style.visibility="visible";
+    document.getElementById("D").innerHTML=d[0][11]
+    document.getElementById("Caption").innerHTML=d[0][10]
+  }else if(s[0][0]=='2'){
+    id.src=d[0][12];
+    id.style.visibility="visible";
+    document.getElementById("D").innerHTML=d[0][14]
+    document.getElementById("Caption").innerHTML=d[0][13]
+  }else if(s[0][0]=='3'){
+    id.src=d[0][15];
+    id.style.visibility="visible";
+    document.getElementById("D").innerHTML=d[0][17]
+    document.getElementById("Caption").innerHTML=d[0][16]
+  }else if(s[0][0]=='4'){
+    id.src=d[0][18];
+    id.style.visibility="visible";
+    document.getElementById("D").innerHTML=d[0][20]
+    document.getElementById("Caption").innerHTML=d[0][19]
+  }else if(s[0][0]=='5'){
+    id.src=d[0][21];
+    id.style.visibility="visible";
+    document.getElementById("D").innerHTML=d[0][23]
+    document.getElementById("Caption").innerHTML=d[0][22]
+  }else if(s[0][0]=='6'){
+    id.src=d[0][24];
+    id.style.visibility="visible";
+    document.getElementById("D").innerHTML=d[0][26]
+    document.getElementById("Caption").innerHTML=d[0][25]
+  }
+}
+
+function set_image(num) {
+  Image=num
+  l=[num]
+  l=l.concat(profiles[n])
+  l=JSON.stringify(l)
+  ajaxPostRequest('/save',l,DONONE)
+}
+function DONONE(){
+  console.log('none')
+}
+
+function Loaded(response){
+  let data = JSON.parse(response);
+  profiles=data
+  SetupProfile()
+}
---
+++ main.py
@@ -0,0 +1,351 @@
+import bottle
+import json
+import csv
+
+@bottle.route("/")
+def index():
+    return bottle.static_file("index.html", root='.')
+@bottle.route("/instagram")
+def instagram():
+    return bottle.static_file("instagram.html", root='.')
+@bottle.route("/icon1.png")
+def icon1():
+    return bottle.static_file("icon1.png", root='.')
+@bottle.route("/Temp_image.png")
+def Temp_image():
+    return bottle.static_file("Temp_image.png", root='.')
+@bottle.route("/play")
+def play():
+    return bottle.static_file("play_screen.html", root='.')
+@bottle.route("/main.js")
+def js():
+    return bottle.static_file("main.js", root='.')
+@bottle.route("/zoom")
+def zoom():
+    return bottle.static_file("image_zoomed.html", root='.')
+@bottle.route('/data')
+def data():
+  o=[]
+  with open('data.csv') as fp:
+    reader=csv.reader(fp)
+    next(reader)
+    for lines in reader:
+      o.append(lines)
+    o = json.dumps(o)
+    print(o)
+    return o
+
+@bottle.post('/save')
+def save():
+  stateInput = bottle.request.body.read().decode()
+  state = json.loads(stateInput)
+  with open('current.csv','w') as fp:
+    writer=csv.writer(fp)
+    writer.writerow(state)
+    return
+@bottle.route('/currentData')
+def Cdata():
+  o=[]
+  with open('current.csv') as fp:
+    reader=csv.reader(fp)
+    for lines in reader:
+      o.append(lines)
+    o = json.dumps(o)
+    print(o)
+    return o
+
+
+
+# not 1
+@bottle.route('/Questions/Type 1 - Bot or Not/not - 1/pfp.jpg')
+def pfp1():
+  return bottle.static_file("Questions/Type 1 - Bot or Not/not - 1/pfp.jpg",root='.')
+@bottle.route('/Questions/Type 1 - Bot or Not/not - 1/post1.png')
+def post11():
+  return bottle.static_file("Questions/Type 1 - Bot or Not/not - 1/post1.png",root='.')
+@bottle.route('/Questions/Type 1 - Bot or Not/not - 1/post2.png')
+def post12():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/not - 1/post2.png",root='.')
+@bottle.route('/Questions/Type 1 - Bot or Not/not - 1/post3.png')
+def post13():
+  return bottle.static_file("Questions/Type 1 - Bot or Not/not - 1/post3.png",root='.')
+@bottle.route('/Questions/Type 1 - Bot or Not/not - 1/post4.png')
+def post14():
+  return bottle.static_file("Questions/Type 1 - Bot or Not/not - 1/post4.png",root='.')
+@bottle.route('/Questions/Type 1 - Bot or Not/not - 1/post5.png')
+def post15():
+  return bottle.static_file("Questions/Type 1 - Bot or Not/not - 1/post5.png",root='.')
+@bottle.route('/Questions/Type 1 - Bot or Not/not - 1/post6.png')
+def post16():
+  return bottle.static_file("Questions/Type 1 - Bot or Not/not - 1/post6.png",root='.')
+
+# not 2
+@bottle.route('/Questions/Type 1 - Bot or Not/not - 2/post1.png')
+def post17():
+  return bottle.static_file("Questions/Type 1 - Bot or Not/not - 2/post1.png",root='.')
+@bottle.route('/Questions/Type 1 - Bot or Not/not - 2/post2.png')
+def post18():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/not - 2/post2.png",root='.')
+@bottle.route('/Questions/Type 1 - Bot or Not/not - 2/post3.png')
+def post19():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/not - 2/post3.png",root='.')
+@bottle.route('/Questions/Type 1 - Bot or Not/not - 2/post3.png')
+def post20():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/not - 2/post4.png",root='.')
+@bottle.route('/Questions/Type 1 - Bot or Not/not - 2/post1.png')
+def post21():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/not - 2/post4.png",root='.')
+@bottle.route('/Questions/Type 1 - Bot or Not/not - 2/post5.png')
+def post22():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/not - 2/post5.png",root='.')
+@bottle.route('/Questions/Type 1 - Bot or Not/not - 2/post6.png')
+def post23():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/not - 2/post6.png",root='.')
+@bottle.route('/Questions/Type 1 - Bot or Not/not - 2/pfp.png')
+def post24():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/not - 2/pfp.png",root='.')
+
+# not 3
+@bottle.route('/Questions/Type 1 - Bot or Not/not - 3/post1.png')
+def post25():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/not - 3/post1.png",root='.')
+  
+@bottle.route('/Questions/Type 1 - Bot or Not/not - 3/post2.png')
+def post26():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/not - 3/post2.png",root='.')
+  
+@bottle.route('/Questions/Type 1 - Bot or Not/not - 3/post3.png')
+def post27():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/not - 3/post3.png",root='.')
+  
+@bottle.route('/Questions/Type 1 - Bot or Not/not - 3/post4.png')
+def post28():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/not - 3/post4.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/not - 3/post5.png')
+def post29():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/not - 3/post5.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/not - 3/post6.png')
+def post30():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/not - 3/post6.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/not - 3/pfp.png')
+def post31():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/not - 3/pfp.png",root='.')
+
+# not 4
+@bottle.route('/Questions/Type 1 - Bot or Not/not - 4/post1.png')
+def post32():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/not - 4/post1.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/not - 4/post2.png')
+def post33():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/not - 4/post2.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/not - 4/post3.png')
+def post34():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/not - 4/post3.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/not - 4/post4.png')
+def post35():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/not - 4/post4.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/not - 4/post5.png')
+def post36():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/not - 4/post5.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/not - 4/post6.png')
+def post37():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/not - 4/post6.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/not - 4/pfp.png')
+def post38():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/not - 4/pfp.png",root='.')
+
+# not 5
+@bottle.route('/Questions/Type 1 - Bot or Not/not - 5/post1.png')
+def post39():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/not - 5/post1.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/not - 5/post2.png')
+def post40():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/not - 5/post2.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/not - 5/post3.png')
+def post41():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/not - 5/post3.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/not - 5/post4.png')
+def post42():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/not - 5/post4.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/not - 5/post5.png')
+def post43():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/not - 5/post5.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/not - 5/post6.png')
+def post44():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/not - 5/post6.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/not - 5/pfp.png')
+def post45():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/not - 5/pfp.png",root='.')
+
+# bot 1
+@bottle.route('/Questions/Type 1 - Bot or Not/bot - 1/post1.png')
+def post46():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/bot - 1/post1.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/bot - 1/post2.png')
+def post47():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/bot - 1/post2.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/bot - 1/post3.png')
+def post48():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/bot - 1/post3.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/bot - 1/post4.png')
+def post49():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/bot - 1/post4.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/bot - 1/post5.png')
+def post50():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/bot - 1/post5.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/bot - 1/post6.png')
+def post51():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/bot - 1/post6.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/bot - 1/pfp.png')
+def post52():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/bot - 1/pfp.png",root='.')
+
+# bot 2
+@bottle.route('/Questions/Type 1 - Bot or Not/bot - 2/post1.png')
+def post53():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/bot - 2/post1.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/bot - 2/post2.png')
+def post54():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/bot - 2/post2.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/bot - 2/post3.png')
+def post55():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/bot - 2/post3.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/bot - 2/post4.png')
+def post56():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/bot - 2/post4.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/bot - 2/post5.png')
+def post57():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/bot - 2/post5.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/bot - 2/post6.png')
+def post58():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/bot - 2/post6.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/bot - 2/pfp.png')
+def post59():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/bot - 2/pfp.png",root='.')
+
+# bot 3
+@bottle.route('/Questions/Type 1 - Bot or Not/bot - 3/post1.png')
+def post60():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/bot - 3/post1.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/bot - 3/post2.png')
+def post61():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/bot - 3/post2.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/bot - 3/post3.png')
+def post62():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/bot - 3/post3.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/bot - 3/post4.png')
+def post63():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/bot - 3/post4.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/bot - 3/post5.png')
+def post64():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/bot - 3/post5.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/bot - 3/post6.png')
+def post65():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/bot - 3/post6.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/bot - 3/pfp.png')
+def post66():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/bot - 3/pfp.png",root='.')
+
+# bot 4
+@bottle.route('/Questions/Type 1 - Bot or Not/bot - 4/post1.png')
+def post67():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/bot - 4/post1.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/bot - 4/post2.png')
+def post68():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/bot - 4/post2.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/bot - 4/post3.png')
+def post69():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/bot - 4/post3.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/bot - 4/post4.png')
+def post70():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/bot - 4/post4.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/bot - 4/post5.png')
+def post71():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/bot - 4/post5.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/bot - 4/post6.png')
+def post72():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/bot - 4/post6.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/bot - 4/pfp.png')
+def post73():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/bot - 4/pfp.png",root='.')
+
+# bot 5
+@bottle.route('/Questions/Type 1 - Bot or Not/bot - 5/post1.png')
+def post74():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/bot - 5/post1.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/bot - 5/post2.png')
+def post75():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/bot - 5/post2.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/bot - 5/post3.png')
+def post76():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/bot - 5/post3.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/bot - 5/post4.png')
+def post77():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/bot - 5/post4.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/bot - 5/post5.png')
+def post78():
+  return bottle.static_file("Questions/Type 1 - Bot or Not/bot - 5/post5.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/bot - 5/post6.png')
+def post79():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/bot - 5/post6.png",root='.')
+
+@bottle.route('/Questions/Type 1 - Bot or Not/bot - 5/pfp.png')
+def post80():
+  return bottle.static_file("/Questions/Type 1 - Bot or Not/bot - 5/pfp.png",root='.')
+
+
+
+
+
+
+
+  
+
+
+
+
+bottle.run(host="0.0.0.0", port=8080, debug=True)
